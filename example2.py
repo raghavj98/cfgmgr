a6162fe9bd7274bc865bb7f6c3ce4f5f03cf88fd
raghavj98 <raghavjindal1998@gmail.com>
2025-02-15 04:27:30 +0530

    Redo interface, add pydantic validation, remove example files from git

--- example2.py
+++
@@ -1,5 +0,0 @@
-from pyconfig import PyConfig
-
-
-def exampleFunc() -> None:
-    print(f"This function uses configb: {PyConfig.get('configb')}")
--- pyconfig.py
+++ pyconfig.py
@@ -1,44 +1,71 @@
-from typing import Any, ClassVar, Callable
+from typing import Any, ClassVar, Callable, TypeAlias, Iterable
+from pydantic import BaseModel
 import json
 import os
 
 
 """TODO
-* Make PyConfig._cfg immutable outside of load function
-* Test how it works when used in imported modules
-* Test post_load_hook
-* Heirarchical config files
+* Make CfgMgr._cfgs immutable outside of load function
+* Introduce a loader class
+    * Instead of taking in args for env, files and kwargs, have a loader class manage this
+    * Loader instances should provide an update method which takes in a dict and updates it with all their keys
+    * Multiple loaders can be provided, will be consumed in order - last one is highest priority
+* Heirarchical config objects
+    * Children inherit all settings from parents
+    * Can override inherited settings, or add new
+    * Parent-child relation can be defined by the name, same as logging.getLogger()
+* 'include' directive in config files
     * Files can include other config files
-    * Settings from included files should be overwritten by main files
+    * Settings from included files should be overwritten by including files
     * Includes can be recursive (check circular includes)
-* Add a validator class
-    * Validator will be called after post_load_hook
-    * Field type validation
-    * Mandatory field check
-    * ...
+    * Idea: Option to provide an include key,
+    *       if this key is present in a config source,
+            it is assumed to point to another config file to be included
 """
 
 
-class PyConfig:
+CfgSchema: TypeAlias = BaseModel
+
+
+class CfgMgr[S: CfgSchema]:
     """Static global container for key value pairs
     Loads configuration options from a file, environment and kwargs passed to PyConfig.load()
     """
 
-    _cfg: ClassVar[dict[str, Any]]
-    _post_load_hook: ClassVar[Callable[[dict], None]] = lambda x: None
+    _cfgs: dict[str, S] = {}
 
-    def __init__(self) -> None:
-        raise NotImplementedError("PyConfig cannot be instantiated")
-    
+    def __init__(self):
+        raise NotImplmentedError
+
     @classmethod
-    def load(cls, cfg_json: str | None = None,
-             env: bool = False,
-             env_prefix: str = '', 
-             loading_order: str = 'fek',
-             **kwargs) -> None:
-        """Load configuration
-        Priority order: kwargs > env > file
+    def getCfg(cls, name: str = '') -> S:
+        """Get an existing configuration
         Args:
+            name: Name passed during loadCfg 
+        Raises:
+            KeyError when config name doesn't exist
+        """
+        return cls._cfgs[name]
+
+    @classmethod
+    def loadCfg(cls, schema: type[S],  # schema is the class S itself (not an object of S)
+                name: str = '',
+                file: str | Iterable[str] | None = None,
+                env: bool = False,
+                env_prefix: str = '', 
+                loading_order: str = 'fek',
+                post_load_hook: Callable[[dict], None] = lambda x: None,
+                **kwargs) -> S:  # So, this function takes the class S and returns an instance of it
+        """Load configuration
+        Args:
+            schema:
+                Pydantic schema to be used for validation
+            name:
+                A label for the generated config
+                To be used with getCfg to fetch the config
+            cfg_json:
+                Config files to read the config from
+
             env:
               Load from env vars
             env_prefix:
@@ -60,58 +87,46 @@ class PyConfig:
             kwargs:
               Additional key value pairs to store in configuration
         """
-        cls._cfg = {}  # Clear any existing config
+        _cfg = cls._load(file, env, env_prefix, loading_order, **kwargs)
+        post_load_hook(_cfg)
+        cfg = schema(**_cfg)
+        cls._cfgs[name] = cfg
+        return cls.getCfg(name)
 
-        cls._load(cfg_json, env, env_prefix, **kwargs)
-
-        _post_load_hook = getattr(cls, '_post_load_hook')
-        _post_load_hook(cls._cfg)
-
-    @classmethod
-    def _load(cls, cfg_json: str | None = None,
+    @staticmethod
+    def _load(file: str | Iterable[str] | None = None,
              env: bool = False,
              env_prefix: str = '', 
              loading_order: str = 'fek',
-             **kwargs) -> None:
+              **kwargs) -> dict[str, Any]:
 
+        _cfg: dict[str, Any] = {}
         for method in loading_order:
-            if method == 'f' and cfg_json is not None:
-                cls._cfg.update(cls._load_json(cfg_json))
+            if method == 'f' and file is not None:
+                if isinstance(file, str):
+                    CfgMgr._update_from_files(_cfg, [file])
+                else:
+                    CfgMgr._update_from_files(_cfg, file)
             if method == 'e' and env:
-                cls._cfg.update(cls._load_from_env(env_prefix))
+                _cfg.update(CfgMgr._load_from_env(env_prefix))
             if method == 'k':
-                cls._cfg.update(kwargs)
+                _cfg.update(kwargs)
+        return _cfg
 
-    @classmethod
-    def _load_from_env(cls, env_prefix: str) -> dict[str, str]:
+    @staticmethod
+    def _update_from_files(cfg: dict[str, Any], files: Iterable[str]) -> None:
+        for file in files:
+            cfg.update(CfgMgr._load_json(file))
+
+    @staticmethod
+    def _load_from_env(env_prefix: str) -> dict[str, str]:
         _ret = {
                 k.removeprefix(env_prefix): v for k, v in os.environ.items()
                 if k.startswith(env_prefix)
         }
         return _ret
 
-    @classmethod
-    def _load_json(cls, fpath: str) -> dict:
+    @staticmethod
+    def _load_json(fpath: str) -> dict[str, Any]:
         with open(fpath) as fp:
             return json.load(fp)
-
-    @classmethod
-    def get(cls, key: str) -> Any:
-        """Return value associated with key
-        """
-        return cls._cfg.get(key)
-
-    @classmethod
-    def to_dict(cls) -> dict:
-        """ Return a dict of loaded configuration options
-        """
-        return cls._cfg
-
-    @classmethod
-    def post_load[F: Callable[[dict], None]](cls, func: F) -> F:
-        """Hook called after configuration is loaded
-        This can be used to validate and modify values, create inferred keys
-        To be used as a decorator on a function that takes in a dict of config key value pairs
-        """
-        cls._post_load_hook = func
-        return func
