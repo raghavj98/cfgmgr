43aeacf79b9071fc88707f18a3ee493f33b73b42
raghavj98 <raghavjindal1998@gmail.com>
2025-02-16 03:06:47 +0530

    Abstract out cfg loading, add yaml support

--- pyconfig.py
+++ pyconfig.py
@@ -1,7 +1,9 @@
-from typing import Any, ClassVar, Callable, TypeAlias, Iterable
+from typing import Any, ClassVar, Callable, TypeAlias, Iterable, TextIO
 from pydantic import BaseModel
 import json
 import os
+from abc import ABC, abstractmethod
+import yaml
 
 
 """TODO
@@ -50,10 +52,7 @@ class CfgMgr[S: CfgSchema]:
     @classmethod
     def loadCfg(cls, schema: type[S],  # schema is the class S itself (not an object of S)
                 name: str = '',
-                file: str | Iterable[str] | None = None,
-                env: bool = False,
-                env_prefix: str = '', 
-                loading_order: str = 'fek',
+                loaders: Iterable[CfgLoader] = (EnvLoader('CFG'),),
                 post_load_hook: Callable[[dict], None] = lambda x: None,
                 **kwargs) -> S:  # So, this function takes the class S and returns an instance of it
         """Load configuration
@@ -63,70 +62,66 @@ class CfgMgr[S: CfgSchema]:
             name:
                 A label for the generated config
                 To be used with getCfg to fetch the config
-            cfg_json:
-                Config files to read the config from
-
-            env:
-              Load from env vars
-            env_prefix:
-              Look only for env vars starting with env_prefix
-              Strip env_prefix from configuration keys
-              Defaults to empty strings (all env vars are stored)
-              No effect if env == False
-            loading_order:
-              Define the priority order of where to pick config vals from
-              'k' -> kwargs as passed to .load()
-              'e' -> environment (as returned by os.environ())
-              'f' -> file as passed to .load()
-              Must be a string formed by the letters 'k', 'e', 'f' in any order
-              Later positon in the string indicates higher priority
-              Example: fek (default) will 
-                1) load from file
-                2) load from environment overriding previous cfg
-                3) load from kwargs overriding previous cfg
+            loaders:
+                An interable of loader objects to load the config vars
+                Defaults to EnvLoader with prefix as "CFG"
             kwargs:
               Additional key value pairs to store in configuration
         """
-        _cfg = cls._load(file, env, env_prefix, loading_order, **kwargs)
+        _cfg = cls._load(loaders, **kwargs)
         post_load_hook(_cfg)
         cfg = schema(**_cfg)
         cls._cfgs[name] = cfg
         return cls.getCfg(name)
 
     @staticmethod
-    def _load(file: str | Iterable[str] | None = None,
-             env: bool = False,
-             env_prefix: str = '', 
-             loading_order: str = 'fek',
-              **kwargs) -> dict[str, Any]:
+    def _load(loaders: Iterable[CfgLoader],  **kwargs) -> dict[str, Any]:
 
         _cfg: dict[str, Any] = {}
-        for method in loading_order:
-            if method == 'f' and file is not None:
-                if isinstance(file, str):
-                    CfgMgr._update_from_files(_cfg, [file])
-                else:
-                    CfgMgr._update_from_files(_cfg, file)
-            if method == 'e' and env:
-                _cfg.update(CfgMgr._load_from_env(env_prefix))
-            if method == 'k':
-                _cfg.update(kwargs)
+        for loader in loaders:
+            loader.update(_cfg)
+        _cfg.update(kwargs)
         return _cfg
 
-    @staticmethod
-    def _update_from_files(cfg: dict[str, Any], files: Iterable[str]) -> None:
-        for file in files:
-            cfg.update(CfgMgr._load_json(file))
 
-    @staticmethod
-    def _load_from_env(env_prefix: str) -> dict[str, str]:
-        _ret = {
-                k.removeprefix(env_prefix): v for k, v in os.environ.items()
-                if k.startswith(env_prefix)
+class CfgLoader(ABC):
+
+    @abstractmethod
+    def __init__(self) -> None:
+        ...
+
+    @property
+    def cfg(self) -> dict[str, Any]:
+        return self._cfg
+
+    @cfg.setter
+    def cfg(self, _cfg: dict[str, Any]) -> None:
+        self._cfg = _cfg
+
+    def update(self, cfg: dict[str, Any]) -> None:
+        cfg.update(self.cfg)
+
+
+class EnvLoader(CfgLoader):
+    
+    def __init__(self, prefix: str = ''):
+        self.cfg = {
+            k.removeprefix(prefix): v for k, v in os.environ.items()
+            if k.startswith(prefix)
         }
-        return _ret
 
-    @staticmethod
-    def _load_json(fpath: str) -> dict[str, Any]:
-        with open(fpath) as fp:
-            return json.load(fp)
+class FileLoader(CfgLoader):
+
+    _loaders: dict[str, Callable[[TextIO], dict[str, Any]]] = {
+            'json': json.load,
+            'yaml': yaml.safe_load
+    }
+
+    def __init__(self, file: str, loader: str | Callable[[TextIO], dict[str, Any]] = 'json'):
+        if isinstance(loader, str) and loader not in self._loaders:
+            raise ValueError(f"Supported loaders are {self._loaders.keys()}")
+        with open(file) as fp:
+            if isinstance(loader, str):
+                self.cfg = self._loaders[loader](fp)
+            else:
+                self.cfg = loader(fp)
