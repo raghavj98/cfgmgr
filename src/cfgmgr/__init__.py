7b9d97907aa4628c640031e2a75cea7aa9a16beb
Raghav Jindal <raghavjindal1998@gmail.com>
2026-05-17 22:40:42 +0530

    Add docs

--- src/cfgmgr/__init__.py
+++ src/cfgmgr/__init__.py
@@ -1,5 +1,13 @@
-'''cfg-mgr: Simple interface for loading and using configurations
-'''
+"""cfg-mgr: Simple interface for loading and using configurations.
+
+Usage:
+    import cfgmgr
+
+    cfgmgr.make_config(env_prefix="CFG_", file_path="config.json")
+    val = cfgmgr.getkey("KEY")  # Reads os.environ["CFG_KEY"]
+    cfgmgr.setkey("KEY", 'a')
+    cfgmgr.get()["KEY"]  # 'a'
+"""
 import os
 import json
 import pathlib
@@ -51,22 +59,27 @@ fileloaders = _FileLoaders()
 
 
 class Loader(Mapping):
-    '''Interface for loaders
-    Override __init__ to load the key value pairs
-    Override get to provide value when asked for a key
-    get() is called by Config.get() so values from a loader can be dynamically constructed
-    Largely just a Mapping, but with a flag to represent mutability of internal data
-    '''
+    """Interface
+
+    A Loader is a Mapping on a data souce for a configuration.
+
+    Attributes:
+        static (bool): True if the underlying data source is mutable.
+            Note: A loader itself is always immutable, but the underlying data source
+            may be mutable through other means (like EnvLoader via os.environ).
+    """
     static = False
 
 
 class EnvLoader(Loader):
+    """Loader for environment variables."""
 
     def __init__(self, prefix=""):
-        '''Load environment variables into config
-        prefix will be stripped from env vars before loading them into the config
-        prefix="" to load all env vars
-        '''
+        """Initialize with environment variables beginning with `prefix`.
+
+        Only variables beginning with `prefix` are exposed through this loader
+        Ex. `prefix=""` to load all env vars
+        """
         self.prefix = prefix
 
     def __getitem__(self, key):
@@ -83,6 +96,10 @@ class EnvLoader(Loader):
 
 
 class FileLoader(Loader):
+    """Abstract base class for loaders reading from a file.
+
+    `FileLoader`s have `static = True` since the stored internal state is immutable.
+    """
     static = True
 
     @abstractmethod
@@ -106,6 +123,10 @@ class FileLoader(Loader):
 
 @fileloaders.register(".json")
 class JSONLoader(FileLoader):
+    """Load JSON files.
+
+    Uses stdlib `json` module.
+    """
 
     @staticmethod
     def _parse(file_path):
@@ -115,6 +136,10 @@ class JSONLoader(FileLoader):
 
 @fileloaders.register(".toml", enabled=tomllib is not None)
 class TOMLLoader(FileLoader):
+    """Load TOML files.
+
+    Requires python>=3.11 since it uses stdlib `tomllib`.
+    """
 
     @staticmethod
     def _parse(file_path):
@@ -124,6 +149,10 @@ class TOMLLoader(FileLoader):
 
 @fileloaders.register(".env", enabled=dotenv is not None)
 class DotEnvLoader(FileLoader):
+    """Load .env files.
+
+    Uses python-dotenv. (`dotenv.dotenv_values`, so `os.environ` is not modified)
+    """
 
     @staticmethod
     def _parse(file_path):
@@ -133,6 +162,10 @@ class DotEnvLoader(FileLoader):
 @fileloaders.register(".yaml", enabled=yaml is not None)
 @fileloaders.register(".yml", enabled=yaml is not None)
 class YAMLLoader(FileLoader):
+    """Load YAML files.
+
+    Uses PyYAML. (`yaml.safe_load`)
+    """
 
     @staticmethod
     def _parse(file_path):
@@ -141,9 +174,19 @@ class YAMLLoader(FileLoader):
 
 
 class IncludeLoader(Loader):
+    """Composite `Loader` for files that includes another file."""
     static = True
 
     def __init__(self, including, included):
+        """Construct a composite `Loader` out of two static `Loader` instances.
+
+        Args:
+            including (Loader): `Loader` for file that includes the other
+            included  (Loader): `Loader` for the file being included
+
+        Raises:
+            TypeError: If either `including` or `included` are non static
+        """
         if not getattr(including, 'static', False):
             raise TypeError(including)
         if not getattr(included, 'static', False):
@@ -177,15 +220,23 @@ class IncludeLoader(Loader):
 
 
 class Config(MutableMapping):
-    # TODO?: Type validation
+    """Primary interface for configuration access
+
+    Composed of an ordered set of `Loader`s. Implements `MutableMapping`.
+    Contained `Loader`s shadow each others' keys.
+    Setting a key using MutableMapping methods shadows all loaders.
+
+    Performance warning: `len` on a `Config` iterates over it (albeit non exhausting)
+    """
     def __init__(self, loaders, **kwargs):
-        '''loaders is an iterable of Loader
-        Loader implements the Mapping interface
-        Config implements the MutableMapping interface
-        Priority order is inverted - a later Loader overrides earlier ones.
-        Ex. If you pass in [FileLoader, EnvLoader], values from EnvLoader will override those from FileLoader
-        **kwargs has highest priority - overrides everything
-        '''
+        """Construct a Config from multiple loaders
+
+        Args:
+            loaders (iterable[Loader]): Source `Loader`s for this `Config`
+                Priority order: later `Loader`s override the ones before them.
+                Ex. If you pass in [FileLoader, EnvLoader], values from EnvLoader will override those from FileLoader
+            **kwargs: Highest priority key value pairs in the `Config` - overrides everything
+        """
         self.loaders = list(loaders)
         self._deleted = set()
         self._overrides = dict(kwargs)
@@ -236,6 +287,25 @@ _config = None
 
 
 def make_config(env_prefix=None, file_path=None, find_file=False, include_key=None, **kwargs):
+    """Initialize a global default `Config`.
+
+    Args:
+        env_prefix (str): If provided, used to construct an `EnvLoader` for the default `Config`.
+        file_path (str): If provided, used to construct a `FileLoader` for the default `Config`.
+            `make_config` will try to auto detect the appropriate `FileLoader` via the file extension.
+        find_file (bool): If `True`, crawl upwards from current working directory until `file_path` is found.
+        include_key (str): Can only be given alongside a `file_path`.
+            If given, the value corresponding to it will be treated as another file to load, recursively.
+        **kwargs: Passed to the `Config` initializer, overrides values from all `Loader`s.
+
+    Raises:
+        ValueError: `include_key` given without `file_path`.
+        FileNotFoundError: `file_path` or any files inferred from `include_key` cannot be found.
+        IncludeCycleError: Cyclical includes detected
+
+    Returns:
+        None
+    """
     global _config
     _loaders = []
     if include_key:
@@ -253,11 +323,29 @@ def make_config(env_prefix=None, file_path=None, find_file=False, include_key=No
 
 
 def get():
+    """Get the default `Config`
+
+    Returns:
+        config (Config): The current global default config
+            Future `make_config` calls will override the current global config.
+            Can be used to "store" configs created by `make_config`.
+    """
     return _config
 
 
 def getkey(key, default=None):
-    # Intentionally not exposed in __all__ to prevent namespace collisions
+    """Get a value from the global default config
+
+    Args:
+        key (str): Passed to `Cofig.get`.
+        default (Optional): If `key` is not found, return this instead.
+
+    Returns:
+        The value corresponding to `key` in the current global default config.
+
+    Raises:
+        AttributeError: If no global default config is initialized (via `make_config`)
+    """
     if _config is None:
         # Log, and let _config.get raise
         _log.error("get called on uninitialized config, did you call make_config?")
@@ -265,7 +353,18 @@ def getkey(key, default=None):
 
 
 def setkey(key, value):
-    # Intentionally not exposed in __all__ to prevent namespace collisions
+    """Set a value in the global default config
+
+    Args:
+        key (str): key to set the value for
+        value (Any): The value to store
+
+    Returns:
+        None
+
+    Raises:
+        AttributeError: If no global default config is initialized (via `make_config`)
+    """
     if _config is None:
         # Log, and let _config.set raise
         _log.error("set called on uninitialized config, did you call make_config?")
@@ -279,6 +378,8 @@ class IncludeCycleError(ValueError):
 # Utility
 
 def deep_merge(prim, sec):
+    """TODO: Docs
+    """
     if not isinstance(prim, Mapping) or not isinstance(sec, Mapping):
         return prim
     _merged = dict(sec)
@@ -288,6 +389,8 @@ def deep_merge(prim, sec):
 
 
 def deep_freeze(val):
+    """TODO: Docs
+    """
     if isinstance(val, Mapping):
         return MappingProxyType({k: deep_freeze(v) for k, v in val.items()})
     if isinstance(val, Sequence) and not isinstance(val, (str, bytes, bytearray)):
@@ -296,6 +399,16 @@ def deep_freeze(val):
 
 
 def resolve_includes(entry_file, include_key='include-cfg'):
+    """Recursively construct loaders for a file with an include directive
+
+    Args:
+        entry_file (str): Path of the entry point file
+        include_key (str): Include key that specifies the files to include.
+            Defaults to 'include-cfg'.
+
+    Returns:
+        List of constructed `Loader`s, in reverse order of inclusion (as required by `Config` initializer).
+    """
     included = list()
     loaders = list()
     curr = find_file_up(entry_file)
@@ -317,6 +430,18 @@ def resolve_includes(entry_file, include_key='include-cfg'):
 
 
 def find_relative_file(base, target):
+    """Find a file path relative to another.
+
+    Args:
+        base (str): File path of the base file.
+        target (str): Path of the target relative to base.
+
+    Returns:
+        Canonical path of the target.
+
+    Raises:
+        FileNotFoundError if no file can be found.
+    """
     if target is None:
         return None
     if os.path.isabs(target) and os.path.isfile(target):
@@ -328,6 +453,17 @@ def find_relative_file(base, target):
 
 
 def find_file_up(basename):
+    """Find a file crawling up from the current working directory
+
+    Args:
+        basename (str): Name of the file to find
+
+    Returns:
+        None if no such file can be found
+
+    Raises:
+        ValueError if file extension is not registered to a corresponding `Loader`
+    """
     # Try to parse extension
     for ext in fileloaders:
         if basename.endswith(ext):
@@ -344,6 +480,19 @@ def find_file_up(basename):
 
 
 def get_file_loader(file_path, /, **kwargs):
+    """Get a constructed loader for a file
+
+    Attempts to find a registered loader for the extension of the given file
+
+    Args:
+        file_path (str): File path to construct a loader from
+
+    Returns:
+        Constructed `Loader` instance for the file
+
+    Raises:
+        ValueError if the extension is not supported
+    """
     for ext in fileloaders:
         if file_path.endswith(ext):
             return fileloaders[ext](file_path, **kwargs)
